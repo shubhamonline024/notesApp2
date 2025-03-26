efaa64267dcc19aaba0ddc806bfae78fa776c137
shubhamonline024 <shubhamonline024@gmail.com>
2025-03-26 01:27:05 +0000

    puppeteer bug fix

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ app.get("/", async (req, res) => {
     }
 
     // Generate PDF from the extracted content
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.setContent(article.content, {
       waitUntil: "networkidle0",
